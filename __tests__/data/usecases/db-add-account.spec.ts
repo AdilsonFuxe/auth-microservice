9108cf940b13cf053441c3be4559ade7936974a7
adilsonfuxe <adilsonfuxe.fuxe@gmail.com>
2021-08-11 06:16:45 +0100

    test: ensure DbAddAccount calls LoadAccountByEmailRepository with correct email

--- __tests__/data/usecases/db-add-account.spec.ts
+++ __tests__/data/usecases/db-add-account.spec.ts
@@ -1,23 +1,45 @@
-import { AddAccountRepository } from '@/src/data/protocols';
+import {
+  AddAccountRepository,
+  LoadAccountByEmailRepository,
+} from '@/src/data/protocols';
 import { DbAddAccount } from '@/src/data/usecases/db-add-account';
 import { mockAddAccountRepositoryStub } from '@/test-suite/data';
+import { mockLoadAccountByEmailRepositoryStub } from '@/test-suite/data/mock-load-account-by-email-repository';
 import { mockAccount, mockAddAccountParams } from '@/test-suite/domain';
 
 type SutTypes = {
   sut: DbAddAccount;
   addAccountRepositoryStub: AddAccountRepository;
+  loadAccountByEmailRepositoryStub: LoadAccountByEmailRepository;
 };
 
 const makeSut = (): SutTypes => {
   const addAccountRepositoryStub = mockAddAccountRepositoryStub();
-  const sut = new DbAddAccount(addAccountRepositoryStub);
+  const loadAccountByEmailRepositoryStub =
+    mockLoadAccountByEmailRepositoryStub();
+  const sut = new DbAddAccount(
+    addAccountRepositoryStub,
+    loadAccountByEmailRepositoryStub
+  );
   return {
     sut,
     addAccountRepositoryStub,
+    loadAccountByEmailRepositoryStub,
   };
 };
 
 describe('DbAddAccount UseCase', () => {
+  it('Should call LoadAccountByEmailRepository with correct email', async () => {
+    const { sut, loadAccountByEmailRepositoryStub } = makeSut();
+    const loadByEmailSpy = jest.spyOn(
+      loadAccountByEmailRepositoryStub,
+      'loadByEmail'
+    );
+    const accountParams = mockAddAccountParams();
+    await sut.add(accountParams);
+    expect(loadByEmailSpy).toHaveBeenCalledWith(accountParams.email);
+  });
+
   it('Should call AddAccountRepository with correct values', async () => {
     const { sut, addAccountRepositoryStub } = makeSut();
     const addSpy = jest.spyOn(addAccountRepositoryStub, 'add');
---
+++ test-suite/data/mock-load-account-by-email-repository.ts
@@ -0,0 +1,15 @@
+import { LoadAccountByEmailRepository } from '@/src/data/protocols';
+import { AccountModel } from '@/src/domain/models';
+import { mockAccount } from '../domain';
+
+export const mockLoadAccountByEmailRepositoryStub =
+  (): LoadAccountByEmailRepository => {
+    class LoadAccountByEmailRepositoryStub
+      implements LoadAccountByEmailRepository
+    {
+      async loadByEmail(): Promise<AccountModel> {
+        return Promise.resolve(mockAccount());
+      }
+    }
+    return new LoadAccountByEmailRepositoryStub();
+  };
