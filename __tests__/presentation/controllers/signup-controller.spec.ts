65cb0d21be33e2c4d94460c35c5b7789be55f690
adilsonfuxe <adilsonfuxe.fuxe@gmail.com>
2021-08-11 05:24:40 +0100

    test: ensure SignUpController calls return 400 if validation fails

--- __tests__/presentation/controllers/signup-controller.spec.ts
+++ __tests__/presentation/controllers/signup-controller.spec.ts
@@ -1,4 +1,6 @@
 import { SignUpController } from '@/src/presentation/controllers/signup-controller';
+import { MissingParamError } from '@/src/presentation/errors';
+import { badRequest } from '@/src/presentation/helpers/http/http-helper';
 import { HttpRequest, Validation } from '@/src/presentation/protocols';
 import { mockValidationStub } from '@/test-suite/presentation';
 
@@ -33,5 +35,14 @@ describe('SignUpController', () => {
     const httpRequest = mockHttpRequest();
     await sut.handle(httpRequest);
     expect(validateSpy).toHaveBeenCalledWith(httpRequest.body);
+  });
+
+  it('Should return 400 if Validation returns an error', async () => {
+    const { sut, validationStub } = makeSut();
+    jest
+      .spyOn(validationStub, 'validate')
+      .mockReturnValueOnce(new MissingParamError('any_field'));
+    const httpResonse = await sut.handle(mockHttpRequest());
+    expect(httpResonse).toEqual(badRequest(new MissingParamError('any_field')));
   });
 });
