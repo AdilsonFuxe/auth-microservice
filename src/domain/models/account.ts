6e994316f0bf7872c97626214d5d90e0083c228f
adilsonfuxe <adilsonfuxe.fuxe@gmail.com>
2021-08-11 02:36:22 +0100

    feat: add account model

---
+++ src/domain/models/account.ts
@@ -0,0 +1,10 @@
+export interface AccountModel {
+  id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  accessToken: string;
+  createdAt: string;
+  updatedAt: string;
+}
