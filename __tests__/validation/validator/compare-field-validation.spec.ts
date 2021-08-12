08b6078cb449ce51bb90b4fa304ae236dddefa75
adilsonfuxe <adilsonfuxe.fuxe@gmail.com>
2021-08-12 03:12:35 +0100

    test: ensure CompareFieldValidation return INvalidParamError if validations fails

---
+++ __tests__/validation/validator/compare-field-validation.spec.ts
@@ -0,0 +1,16 @@
+import { InvalidParamError } from '@/src/presentation/errors';
+import { CompareFieldValidation } from '@/src/validation/validators';
+
+const makeSut = (): CompareFieldValidation =>
+  new CompareFieldValidation('password', 'passwordConfirmation');
+
+describe('CompareFieldValidation', () => {
+  it('Should returns a InvalidParamError if Validation fails', () => {
+    const sut = makeSut();
+    const error = sut.validate({
+      password: '12354',
+      passwordConfirmation: '12345',
+    });
+    expect(error).toEqual(new InvalidParamError('passwordConfirmation'));
+  });
+});
