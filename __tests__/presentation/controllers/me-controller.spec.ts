0c9a5f54ed818d0d0f8e8e67ecd1c0c329bfbfc7
adilsonfuxe <adilsonfuxe.fuxe@gmail.com>
2021-08-12 12:39:05 +0100

    test: ensure MeController return 500 if LoadAccountById throw

--- __tests__/presentation/controllers/me-controller.spec.ts
+++ __tests__/presentation/controllers/me-controller.spec.ts
@@ -1,8 +1,12 @@
 import { LoadAccountById } from '@src/domain/usecases/load-account-by-id';
 import { MeController } from '@src/presentation/controllers/me-controller';
 import { AccessDeniedError } from '@src/presentation/errors';
-import { forbidden } from '@src/presentation/helpers/http/http-helper';
+import {
+  forbidden,
+  serverError,
+} from '@src/presentation/helpers/http/http-helper';
 import { HttpRequest } from '@src/presentation/protocols';
+import { trhowError } from '@test-suite/helper';
 import { mockLoadAccountById } from '@test-suite/presentation';
 
 const mockHttpRequest = (): HttpRequest => ({
@@ -38,5 +42,14 @@ describe('MeController', () => {
       .mockReturnValueOnce(Promise.resolve(null));
     const httpResponse = await sut.handle(mockHttpRequest());
     expect(httpResponse).toEqual(forbidden(new AccessDeniedError()));
+  });
+
+  it('Should return 500 if LoadAccountById throws', async () => {
+    const { sut, loadAccountByIdStub } = makeSut();
+    jest
+      .spyOn(loadAccountByIdStub, 'loadById')
+      .mockImplementationOnce(trhowError);
+    const httpResponse = await sut.handle(mockHttpRequest());
+    expect(httpResponse).toEqual(serverError(new Error()));
   });
 });
