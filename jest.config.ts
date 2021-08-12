d4efa31acfdbb5d0b58d55b3560cc3c61feb40d3
adilsonfuxe <adilsonfuxe.fuxe@gmail.com>
2021-08-12 05:27:11 +0100

    chore: update jest config

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,7 @@
 export default {
   roots: ['<rootDir>/__tests__'],
   clearMocks: true,
-  collectCoverageFrom: ['<rootDir>/src/**/*.{ts}'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
   testEnvironment: 'node',
