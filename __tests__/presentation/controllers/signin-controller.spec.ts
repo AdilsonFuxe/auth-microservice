98895b1c77f82e7f04efd1c577371058599ee7de
adilsonfuxe <adilsonfuxe.fuxe@gmail.com>
2021-08-12 05:47:45 +0100

    test: ensure SignInController return 400 if Validation return an error

--- __tests__/presentation/controllers/signin-controller.spec.ts
+++ __tests__/presentation/controllers/signin-controller.spec.ts
@@ -1,4 +1,6 @@
 import { SignInController } from '@src/presentation/controllers/signin-controller';
+import { MissingParamError } from '@src/presentation/errors';
+import { badRequest } from '@src/presentation/helpers/http/http-helper';
 import { HttpRequest, Validation } from '@src/presentation/protocols';
 import { mockValidationStub } from '@test-suite/presentation';
 
@@ -30,5 +32,14 @@ describe('SignIn Controller', () => {
     const httpRequest = mockHttpRequest();
     await sut.handle(httpRequest);
     expect(validateSpy).toHaveBeenCalledWith(httpRequest.body);
+  });
+
+  it('Should return 400 if Validation returns an error', async () => {
+    const { sut, validationStub } = makeSut();
+    jest
+      .spyOn(validationStub, 'validate')
+      .mockReturnValueOnce(new MissingParamError('any_field'));
+    const httpResonse = await sut.handle(mockHttpRequest());
+    expect(httpResonse).toEqual(badRequest(new MissingParamError('any_field')));
   });
 });
