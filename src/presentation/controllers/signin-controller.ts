0f404f935e6a32ea557e862603c0e9de690a8ab1
adilsonfuxe <adilsonfuxe.fuxe@gmail.com>
2021-08-12 05:47:23 +0100

    feat: ensure SignInController return 400 if Validation return an error

--- src/presentation/controllers/signin-controller.ts
+++ src/presentation/controllers/signin-controller.ts
@@ -4,13 +4,16 @@ import {
   HttpResponse,
   Validation,
 } from '@src/presentation/protocols';
-import { serverError } from '../helpers/http/http-helper';
+import { badRequest, serverError } from '../helpers/http/http-helper';
 
 export class SignInController implements Controller {
   constructor(private readonly validation: Validation) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      this.validation.validate(httpRequest.body);
+      const error = this.validation.validate(httpRequest.body);
+      if (error) {
+        return badRequest(error);
+      }
     } catch (error) {
       return serverError(error);
     }
