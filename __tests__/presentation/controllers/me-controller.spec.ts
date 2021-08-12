6e404638a2cc9a3cda00823985100debf8ef0a7b
adilsonfuxe <adilsonfuxe.fuxe@gmail.com>
2021-08-12 12:38:01 +0100

    test: ensure MeController return 403 if LoadAccountById return null

--- __tests__/presentation/controllers/me-controller.spec.ts
+++ __tests__/presentation/controllers/me-controller.spec.ts
@@ -1,5 +1,7 @@
 import { LoadAccountById } from '@src/domain/usecases/load-account-by-id';
 import { MeController } from '@src/presentation/controllers/me-controller';
+import { AccessDeniedError } from '@src/presentation/errors';
+import { forbidden } from '@src/presentation/helpers/http/http-helper';
 import { HttpRequest } from '@src/presentation/protocols';
 import { mockLoadAccountById } from '@test-suite/presentation';
 
@@ -27,5 +29,14 @@ describe('MeController', () => {
     const loadSpy = jest.spyOn(loadAccountByIdStub, 'loadById');
     await sut.handle(mockHttpRequest());
     expect(loadSpy).toHaveBeenCalledWith('account_id');
+  });
+
+  it('Should return 403 if LoadAccountById returns null', async () => {
+    const { sut, loadAccountByIdStub } = makeSut();
+    jest
+      .spyOn(loadAccountByIdStub, 'loadById')
+      .mockReturnValueOnce(Promise.resolve(null));
+    const httpResponse = await sut.handle(mockHttpRequest());
+    expect(httpResponse).toEqual(forbidden(new AccessDeniedError()));
   });
 });
