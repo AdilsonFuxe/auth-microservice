92ab8ccdccdef88b30812bf226330a5ae504b7c2
adilsonfuxe <adilsonfuxe.fuxe@gmail.com>
2021-08-12 05:51:15 +0100

    test: ensure SignInController calls Authentication with correct values

--- __tests__/presentation/controllers/signin-controller.spec.ts
+++ __tests__/presentation/controllers/signin-controller.spec.ts
@@ -1,8 +1,12 @@
+import { Authentication } from '@src/domain/usecases';
 import { SignInController } from '@src/presentation/controllers/signin-controller';
 import { MissingParamError } from '@src/presentation/errors';
 import { badRequest } from '@src/presentation/helpers/http/http-helper';
 import { HttpRequest, Validation } from '@src/presentation/protocols';
-import { mockValidationStub } from '@test-suite/presentation';
+import {
+  mockAuthenticationStub,
+  mockValidationStub,
+} from '@test-suite/presentation';
 
 const mockHttpRequest = (): HttpRequest => ({
   body: {
@@ -14,14 +18,17 @@ const mockHttpRequest = (): HttpRequest => ({
 type SutTypes = {
   sut: SignInController;
   validationStub: Validation;
+  authenticationStub: Authentication;
 };
 
 const makeSut = (): SutTypes => {
   const validationStub = mockValidationStub();
-  const sut = new SignInController(validationStub);
+  const authenticationStub = mockAuthenticationStub();
+  const sut = new SignInController(validationStub, authenticationStub);
   return {
     sut,
     validationStub,
+    authenticationStub,
   };
 };
 
@@ -41,5 +48,15 @@ describe('SignIn Controller', () => {
       .mockReturnValueOnce(new MissingParamError('any_field'));
     const httpResonse = await sut.handle(mockHttpRequest());
     expect(httpResonse).toEqual(badRequest(new MissingParamError('any_field')));
+  });
+
+  test('Should call Authentication with correct values', async () => {
+    const { sut, authenticationStub } = makeSut();
+    const authSpy = jest.spyOn(authenticationStub, 'auth');
+    await sut.handle(mockHttpRequest());
+    expect(authSpy).toHaveBeenCalledWith({
+      email: 'any_email@mail.com',
+      password: 'any_password',
+    });
   });
 });
