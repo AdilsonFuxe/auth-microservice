7469828c0cb4a7f6c8b128554e90f3808f24f2d0
adilsonfuxe <adilsonfuxe.fuxe@gmail.com>
2021-08-12 02:30:12 +0100

    test: ensure AccountMongoRepository update accessToken on success

---
+++ __tests__/infra/db/mongoose/repositories/update-access-token-repository.spec.ts
@@ -0,0 +1,27 @@
+import { MongoHelper } from '@/src/infra/db/mongoose/helper/mongo-helper';
+import { mockAddAccountParams } from '@/test-suite/domain';
+import { AccountMongooseModel } from '@/src/infra/db/mongoose/models';
+import { AccountMongoRepository } from '@/src/infra/db/mongoose/repositories';
+import faker from 'faker';
+
+describe('UpdateAccessTokenMongoRepository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL as string);
+  });
+
+  afterAll(async () => {
+    await AccountMongooseModel.deleteMany({});
+    await MongoHelper.disconnect();
+  });
+
+  it('Should update accessToken on success', async () => {
+    const sut = new AccountMongoRepository();
+    const params = mockAddAccountParams();
+    const account = await AccountMongooseModel.create(params);
+    expect(account.accessToken).toBeFalsy();
+    const accessToken = faker.datatype.uuid();
+    await sut.updateAccessToken(account.id, accessToken);
+    const result = await AccountMongooseModel.findById(account.id);
+    expect(result.accessToken).toBe(accessToken);
+  });
+});
