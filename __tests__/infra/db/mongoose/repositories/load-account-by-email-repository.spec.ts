03705fcd4eaa47828198903d4f394282f8d22e52
adilsonfuxe <adilsonfuxe.fuxe@gmail.com>
2021-08-12 02:11:03 +0100

    test: ensure AccountMongoRepository return an account on loadByEmail Success

--- __tests__/infra/db/mongoose/repositories/load-account-by-email-repository.spec.ts
+++ __tests__/infra/db/mongoose/repositories/load-account-by-email-repository.spec.ts
@@ -1,6 +1,7 @@
 import { MongoHelper } from '@/src/infra/db/mongoose/helper/mongo-helper';
 import { AccountMongooseModel } from '@/src/infra/db/mongoose/models';
 import { AccountMongoRepository } from '@/src/infra/db/mongoose/repositories';
+import { mockAddAccountParams } from '@/test-suite/domain';
 import faker from 'faker';
 
 const makeSut = () => new AccountMongoRepository();
@@ -22,5 +23,18 @@ describe('LoadAccountByEmailMongoRepository', () => {
     const sut = makeSut();
     const account = await sut.loadByEmail(faker.internet.email());
     expect(account).toBeNull();
+  });
+
+  it('Should return an account on loadByEmail success', async () => {
+    const sut = makeSut();
+    const params = mockAddAccountParams();
+    await AccountMongooseModel.create(params);
+    const account = await sut.loadByEmail(params.email);
+    expect(account).toBeTruthy();
+    expect(account.id).toBeTruthy();
+    expect(account.firstName).toBe(params.firstName);
+    expect(account.lastName).toBe(params.lastName);
+    expect(account.email).toBe(params.email);
+    expect(account.password).toBe(params.password);
   });
 });
