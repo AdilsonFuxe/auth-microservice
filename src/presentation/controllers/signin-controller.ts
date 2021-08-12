891bf8ae2422ad7e8a47803ea61365aec8f9ea9e
adilsonfuxe <adilsonfuxe.fuxe@gmail.com>
2021-08-12 05:50:56 +0100

    feat: ensure SignInController calls Authentication with correct values

--- src/presentation/controllers/signin-controller.ts
+++ src/presentation/controllers/signin-controller.ts
@@ -1,3 +1,4 @@
+import { Authentication } from '@src/domain/usecases';
 import {
   Controller,
   HttpRequest,
@@ -7,13 +8,21 @@ import {
 import { badRequest, serverError } from '../helpers/http/http-helper';
 
 export class SignInController implements Controller {
-  constructor(private readonly validation: Validation) {}
+  constructor(
+    private readonly validation: Validation,
+    private readonly authentication: Authentication
+  ) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const error = this.validation.validate(httpRequest.body);
       if (error) {
         return badRequest(error);
       }
+      const { email, password } = httpRequest.body;
+      await this.authentication.auth({
+        email,
+        password,
+      });
     } catch (error) {
       return serverError(error);
     }
