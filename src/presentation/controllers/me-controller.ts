efd4ecdd34cf5a351b1264ceecd0507effb8aaef
adilsonfuxe <adilsonfuxe.fuxe@gmail.com>
2021-08-12 12:37:44 +0100

    feat: ensure MeController return 403 if LoadAccountById return null

--- src/presentation/controllers/me-controller.ts
+++ src/presentation/controllers/me-controller.ts
@@ -3,15 +3,24 @@ import {
   HttpRequest,
   HttpResponse,
 } from '@src/presentation/protocols';
-import { serverError } from '@src/presentation/helpers/http/http-helper';
+import {
+  forbidden,
+  serverError,
+} from '@src/presentation/helpers/http/http-helper';
 import { LoadAccountById } from '@src/domain/usecases/load-account-by-id';
+import { AccessDeniedError } from '../errors';
 
 export class MeController implements Controller {
   constructor(private readonly loadAccountById: LoadAccountById) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      await this.loadAccountById.loadById(httpRequest.accountId);
+      const account = await this.loadAccountById.loadById(
+        httpRequest.accountId
+      );
+      if (!account) {
+        return forbidden(new AccessDeniedError());
+      }
     } catch (error) {
       return serverError(error);
     }
--- src/presentation/errors/index.ts
+++ src/presentation/errors/index.ts
@@ -3,3 +3,4 @@ export * from './contact-in-use-error';
 export * from './server-error';
 export * from './invalid-param-error';
 export * from './unauthorized-error';
+export * from './access-denied-error';
