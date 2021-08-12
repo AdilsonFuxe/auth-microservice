1162eb3d34df5e2fda81be97bf3fd2808619dd53
adilsonfuxe <adilsonfuxe.fuxe@gmail.com>
2021-08-12 02:06:05 +0100

    feat: ensure AccountMongoRepository return null account loaded not found

--- src/infra/db/mongoose/repositories/account-mongo-repository.ts
+++ src/infra/db/mongoose/repositories/account-mongo-repository.ts
@@ -1,14 +1,24 @@
-import { AddAccountRepository } from '@/src/data/protocols';
+import {
+  AddAccountRepository,
+  LoadAccountByEmailRepository,
+} from '@/src/data/protocols';
 import { AccountModel } from '@/src/domain/models';
 import { AddAccountParams } from '@/src/domain/usecases';
 import { AccountMongooseModel } from '@/src/infra/db/mongoose/models';
 import { MongoHelper } from '@/src/infra/db/mongoose/helper/mongo-helper';
 
-export class AccountMongoRepository implements AddAccountRepository {
+export class AccountMongoRepository
+  implements AddAccountRepository, LoadAccountByEmailRepository
+{
   async add(params: AddAccountParams): Promise<AccountModel> {
     const doc = new AccountMongooseModel(params);
     await doc.save();
     const parsedDoc = JSON.parse(JSON.stringify(doc));
     return MongoHelper.serialize(parsedDoc);
+  }
+
+  async loadByEmail(email: string): Promise<AccountModel> {
+    const account = await AccountMongooseModel.findOne({ email }).lean();
+    return MongoHelper.serialize(account);
   }
 }
