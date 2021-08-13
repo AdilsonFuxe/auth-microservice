a512d1d362c7899e85842e9160eadc6d935e9f7a
adilsonfuxe <adilsonfuxe.fuxe@gmail.com>
2021-08-13 07:33:51 +0100

    test: ensure forgot route return 204 on forgot assword success

---
+++ __tests__/main/routes/forgot-route.test.ts
@@ -0,0 +1,30 @@
+import request from 'supertest';
+import app from '@src/main/config/app';
+import { MongoHelper } from '@src/infra/db/mongoose/helper/mongo-helper';
+import { AccountMongooseModel } from '@src/infra/db/mongoose/models';
+import { mockCreateAccountOnDb } from '@test-suite/helper';
+
+jest.mock('nodemailer', () => ({
+  createTransport: jest.fn().mockReturnValue({
+    sendMail: jest.fn().mockReturnValue((mailoptions, callback) => {}),
+  }),
+}));
+
+describe('Patch /forgot', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL as string);
+  });
+
+  beforeEach(async () => {
+    await AccountMongooseModel.deleteMany({});
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  test('Should return 204 on forgot password success', async () => {
+    const { email } = await mockCreateAccountOnDb();
+    await request(app).patch('/api/v1/forgot').send({ email }).expect(204);
+  });
+});
