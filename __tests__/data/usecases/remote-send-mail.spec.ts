265b66d39ae3943f5d32032e546e237a9748d4da
adilsonfuxe <adilsonfuxe.fuxe@gmail.com>
2021-08-13 01:13:09 +0100

    test: ensure RemoteSendMail throw if nodeMailerSendMail throws

--- __tests__/data/usecases/remote-send-mail.spec.ts
+++ __tests__/data/usecases/remote-send-mail.spec.ts
@@ -2,6 +2,7 @@ import { NodeMailerSendMail } from '@src/data/protocols';
 import { RemoteSendMail } from '@src/data/usecases/remote-send-mail';
 import { mockNodeMailerSendMail } from '@test-suite/data';
 import { mockSendMailParams } from '@test-suite/domain';
+import { trhowError } from '@test-suite/helper';
 
 type SutTypes = {
   sut: RemoteSendMail;
@@ -24,5 +25,14 @@ describe('RemoteSendMail Usecase', () => {
     const params = mockSendMailParams();
     await sut.sendMail(params);
     expect(sendMailSpy).toHaveBeenCalledWith(params);
+  });
+
+  it('Should throw if NodeMailerSendMail throws', async () => {
+    const { sut, nodeMailerSendMailStub } = makeSut();
+    jest
+      .spyOn(nodeMailerSendMailStub, 'sendMail')
+      .mockImplementationOnce(trhowError);
+    const promise = sut.sendMail(mockSendMailParams());
+    await expect(promise).rejects.toThrow();
   });
 });
