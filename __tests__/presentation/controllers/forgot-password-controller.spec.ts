5fb5413eff74be098fe51b191eb98078fae75779
adilsonfuxe <adilsonfuxe.fuxe@gmail.com>
2021-08-13 05:29:24 +0100

    test: ensure ForgotPasswordController return 400 if Validation fails

--- __tests__/presentation/controllers/forgot-password-controller.spec.ts
+++ __tests__/presentation/controllers/forgot-password-controller.spec.ts
@@ -1,4 +1,6 @@
 import { ForgotPasswordController } from '@src/presentation/controllers/forgot-password-controller';
+import { MissingParamError } from '@src/presentation/errors';
+import { badRequest } from '@src/presentation/helpers/http/http-helper';
 import { HttpRequest, Validation } from '@src/presentation/protocols';
 import { mockValidationStub } from '@test-suite/presentation';
 
@@ -29,5 +31,14 @@ describe('ForgotPassword Controller', () => {
     const httpRequest = mockHttpRequest();
     await sut.handle(httpRequest);
     expect(validateSpy).toHaveBeenCalledWith(httpRequest.body);
+  });
+
+  it('Should return 400 if Validation returns an error', async () => {
+    const { sut, validationStub } = makeSut();
+    jest
+      .spyOn(validationStub, 'validate')
+      .mockReturnValueOnce(new MissingParamError('any_field'));
+    const httpResonse = await sut.handle(mockHttpRequest());
+    expect(httpResonse).toEqual(badRequest(new MissingParamError('any_field')));
   });
 });
