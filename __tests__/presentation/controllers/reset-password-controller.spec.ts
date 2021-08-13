c9c51301ae9306e149a6f9ec442c17fd1b4bfd4b
adilsonfuxe <adilsonfuxe.fuxe@gmail.com>
2021-08-13 11:00:31 +0100

    test: ensure ResetPasswordController calls LoadAccountByEmail with correct email

--- __tests__/presentation/controllers/reset-password-controller.spec.ts
+++ __tests__/presentation/controllers/reset-password-controller.spec.ts
@@ -1,8 +1,12 @@
+import { LoadAccountByEmail } from '@src/domain/usecases/load-account-by-email';
 import { ResetPasswordController } from '@src/presentation/controllers/reset-password-controller';
 import { MissingParamError } from '@src/presentation/errors';
 import { badRequest } from '@src/presentation/helpers/http/http-helper';
 import { HttpRequest, Validation } from '@src/presentation/protocols';
-import { mockValidationStub } from '@test-suite/presentation';
+import {
+  mockLoadAccountByEmail,
+  mockValidationStub,
+} from '@test-suite/presentation';
 
 const mockHttpRequest = (): HttpRequest => ({
   body: {
@@ -15,14 +19,20 @@ const mockHttpRequest = (): HttpRequest => ({
 type SutTypes = {
   sut: ResetPasswordController;
   validationStub: Validation;
+  loadAccountByEmailStub: LoadAccountByEmail;
 };
 
 const makeSut = (): SutTypes => {
   const validationStub = mockValidationStub();
-  const sut = new ResetPasswordController(validationStub);
+  const loadAccountByEmailStub = mockLoadAccountByEmail();
+  const sut = new ResetPasswordController(
+    validationStub,
+    loadAccountByEmailStub
+  );
   return {
     sut,
     validationStub,
+    loadAccountByEmailStub,
   };
 };
 
@@ -42,5 +52,13 @@ describe('ReserPassword Controller', () => {
       .mockReturnValueOnce(new MissingParamError('any_field'));
     const httpResonse = await sut.handle(mockHttpRequest());
     expect(httpResonse).toEqual(badRequest(new MissingParamError('any_field')));
+  });
+
+  it('Should call LoadAccountBYEmail with correct email', async () => {
+    const { sut, loadAccountByEmailStub } = makeSut();
+    const loadByEmailSpy = jest.spyOn(loadAccountByEmailStub, 'loadByEmail');
+    const httpRequest = mockHttpRequest();
+    await sut.handle(httpRequest);
+    expect(loadByEmailSpy).toHaveBeenCalledWith('any_email@mail.com');
   });
 });
