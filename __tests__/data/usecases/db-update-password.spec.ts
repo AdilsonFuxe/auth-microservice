1118f9c6934d7656af2e37d1c255da8de5d13b25
adilsonfuxe <adilsonfuxe.fuxe@gmail.com>
2021-08-13 10:24:29 +0100

    test: ensure DbUpdatePassword throw if UpdatePasswordRepository throws

--- __tests__/data/usecases/db-update-password.spec.ts
+++ __tests__/data/usecases/db-update-password.spec.ts
@@ -1,6 +1,7 @@
 import { UpdatePasswordRepository } from '@src/data/protocols';
 import { DbUpdatePassword } from '@src/data/usecases/db-update-password';
 import { mockUpdatePasswordRepository } from '@test-suite/data';
+import { trhowError } from '@test-suite/helper';
 
 type SutTypes = {
   sut: DbUpdatePassword;
@@ -25,5 +26,14 @@ describe('UpdatePassword Usecase', () => {
     );
     await sut.updatePasseword('any_id', 'new_password');
     expect(updatePasswordSpy).toHaveBeenCalledWith('any_id', 'new_password');
+  });
+
+  it('Should throw if Decrypter throws', async () => {
+    const { sut, updatePasswordRepository } = makeSut();
+    jest
+      .spyOn(updatePasswordRepository, 'updatePassword')
+      .mockImplementationOnce(trhowError);
+    const promise = sut.updatePasseword('any_id', 'new_password');
+    await expect(promise).rejects.toThrow();
   });
 });
