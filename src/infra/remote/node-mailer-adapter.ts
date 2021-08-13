e63de7d2a393fbe9d69a6e9449cd12a100846915
adilsonfuxe <adilsonfuxe.fuxe@gmail.com>
2021-08-13 01:42:56 +0100

    feat: ensure NodeMailerAdapter calls createTransport with correct values

---
+++ src/infra/remote/node-mailer-adapter.ts
@@ -0,0 +1,22 @@
+import { NodeMailerSendMail } from '@src/data/protocols';
+import { SendMailParams } from '@src/domain/usecases/send-mail';
+import nodemailer from 'nodemailer';
+
+export class NodeMailerAdapter implements NodeMailerSendMail {
+  constructor(
+    private readonly host: string,
+    private readonly port: number,
+    private readonly user: string,
+    private readonly pass: string
+  ) {}
+  async sendMail(params: SendMailParams): Promise<void> {
+    const transport = nodemailer.createTransport({
+      host: this.host,
+      port: this.port,
+      auth: {
+        user: this.user,
+        pass: this.pass,
+      },
+    });
+  }
+}
