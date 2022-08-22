7ffebeeadc6ccb229f35fd2a5b3f384cb5a3fd47
adilsonfuxe <adilsonfuxe.fuxe@gmail.com>
2022-08-22 11:03:36 +0100

    test: ensure dbSignout throw if signoutRepsotory throws

--- __tests__/data/usecases/db-signout.spec.ts
+++ __tests__/data/usecases/db-signout.spec.ts
@@ -1,16 +1,26 @@
-import { BuildDbSignoutRepository } from '@src/data/usecases/db-signout/protocols';
+import { dbSignout } from '@src/data/usecases';
 import { mockSignoutRepository } from '@test-suite/data';
+
+const makeSut = () => {
+  const signoutRepository = jest.fn(mockSignoutRepository());
+  const sut = dbSignout({ signoutRepository });
+  return {
+    sut,
+    signoutRepository,
+  };
+};
 
 describe('Signout Usecases', () => {
   it('Should call signoutRepository with correct accountId', async () => {
-    const dbSignout: BuildDbSignoutRepository =
-      ({ signoutRepository }) =>
-      async (accountId: string) => {
-        await signoutRepository(accountId);
-      };
-    const signoutRepository = jest.fn(mockSignoutRepository());
-    const sut = dbSignout({ signoutRepository });
+    const { sut, signoutRepository } = makeSut();
     await sut('any_account_id');
     expect(signoutRepository).toHaveBeenCalledWith('any_account_id');
+  });
+
+  it('Should throw if signoutRepository throws', async () => {
+    const { sut, signoutRepository } = makeSut();
+    signoutRepository.mockRejectedValue(new Error());
+    const promise = sut('any_account_id');
+    await expect(promise).rejects.toThrow();
   });
 });
